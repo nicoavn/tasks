403a494c53479287e8d64f23bfd45ce16289a3ce
Alvin <nicoavn@gmail.com>
2021-08-21 23:22:08 -0400

    Tasks API

--- api/views.py
+++ api/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
-from django.core import serializers
-from django.http import HttpResponse
+from django.http import JsonResponse
 from models.models import Task
-# Create your views here.
+
 
 def task_list(request):
     tasks = Task.objects.filter().order_by('-created_at')
-    task_list = serializers.serialize('json', tasks)
-    return HttpResponse(task_list, content_type="text/json-comment-filtered")
+    return JsonResponse([t.as_dict() for t in tasks], safe=False)
--- models/models.py
+++ models/models.py
@@ -1,12 +1,23 @@
 import uuid
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
 
 class Task(models.Model):
 	task_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	description = models.CharField(max_length=254, null=True)
 	task_type = models.CharField(max_length=40)
-	created_at = models.DateTimeField(auto_now=True)
-	updated_at = models.DateTimeField(auto_now=True)
-	archived_at = models.DateTimeField(null=True)
+	created_at = models.DateTimeField(blank=True, null=True, editable=False)
+	updated_at = models.DateTimeField(blank=True, null=True, editable=False)
+	archived_at = models.DateTimeField(blank=True, null=True)
+
+	def save(self, *args, **kwargs):
+		if not self.task_id:
+			self.created_at = datetime.now()
+		super(Task, self).save(*args, **kwargs)
+
+	def __str__(self):
+		return self.description
+
+	def as_dict(self):
+		return dict((f.name, getattr(self, f.name)) for f in self._meta.fields)
